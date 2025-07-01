09c11fd2ab4b6d4e3c0b92f513c4dd0da397888d
celiogouvea <celiogomesgouvea@gmail.com>
2025-07-01 09:34:43 -0300

    Modificação do contexto do sistema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                     <div class="col-lg-5 col-md-6">
                         <h1 class="text-white">{{ __('Welcome!') }}</h1>
                         <p class="text-lead text-light">
-                            {{ __('Use White Dashboard theme to create a great project.') }}
+                            {{ __('System for creating individual and secure news.') }}
                         </p>
                     </div>
                 </div>
